7b9e3ec0c06ebd2e4d7525f8c42c2b5988162e4d
Stiven Trujillo <lstrujillo@incolmotos-yamaha.com.co>
2025-07-28 20:58:45 -0500

    [ADD] Here be Dragons

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # stiven-random-str-msg
+
 Random String Messages
+
+## Install
+
+```npm
+npm  install -g stiven-random-str-msg
+```
+
+# Usage
+
+```
+stiven-random-str-msg
+```
+
+# Contributing
+
+If someone wants to add or improve something, I invite you to collaborate directly in this repository: [stiven-random-str-msg](https://github.com/)
+
+# License
+
+stiven-random-str-msg is released under the [MIT License](https://opensource.org/licenses/MIT).
---
+++ src/index.js
@@ -0,0 +1,29 @@
+const messages = [
+  'This is where it all begins...',
+  'Commit committed',
+  'Version control is awful',
+  'COMMIT ALL THE FILES!',
+  'The same thing we do every night, Pinky - try to take over the world!',
+  'Lock S-foils in attack position',
+  'This commit is a lie',
+  "I'll explain when you're older!",
+  'Here be Dragons',
+  'Reinventing the wheel. Again.',
+  'This is not the commit message you are looking for',
+  'Batman! (this commit has no parents)',
+];
+
+const funnyCommit = () => {
+  // Genera un número aleatorio entre 0 y 1, lo multiplica por la longitud del array
+  // y usa Math.floor para obtener un índice entero válido
+  const message = messages[Math.floor(Math.random() * messages.length)];
+  
+  // Imprime el mensaje usando códigos ANSI para colores:
+  // \x1b[34m - establece el color de texto a azul
+  // \x1b[89m - restablece el color al valor por defecto
+  console.log(`\x1b[34m${message}\x1b[89m`);
+};
+
+module.exports = {
+  funnyCommit,
+};
